ee785bffee4dfe75ed34ece9e0f80aced8c28736
yes-lonso <alonsomoiron@gmail.com>
2025-01-05 20:17:53 +0100

    Sirviendo contenido estático

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
 import { LibrosModule } from './libros/libros.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { PrestamosModule } from './prestamos/prestamos.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TestDataModule } from './test-data/test-data.module';
 
 @Module({
@@ -10,6 +12,7 @@ import { TestDataModule } from './test-data/test-data.module';
       MongooseModule.forRoot(
          'mongodb+srv://giin21:giin21@giin.ei7gf.mongodb.net/biblioteca'
       ),
+      ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
       LibrosModule,
       UsuariosModule,
       PrestamosModule,
